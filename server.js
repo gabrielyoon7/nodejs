66cc876406ef7434a6258c1f64244c3830833129
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-01-31 14:59:32 +0900

    query string

--- server.js
+++ server.js
@@ -12,3 +12,13 @@ app.get('/pet', function(요청, 응답){
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname + '/index.html');
 });
+
+app.get('/random.text', function (req, res) {
+    res.send('random.text');
+  });
+
+//쿼리 스트링 받는 방법
+app.get('/topic', function (req, res) {
+    let topic = ['안녕','하세요','반갑습니다']
+    res.send(topic[req.query.id]);
+})
